16861d1db416d0fc9ab95894aee727e5ee0fcc6f
leocs000 <leocsilva000@outlook.com>
2024-05-21 23:14:28 -0300

    Criando o projeto

---
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estado-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './estado-list.component.html',
+  styleUrl: './estado-list.component.css'
+})
+export class EstadoListComponent {
+
+}
