f226ff9fba7e6df91ce22c9bba24c9fe6ea77a64
leocs000 <leocsilva000@outlook.com>
2024-05-22 07:35:01 -0300

    adicionando funcao de salvar novo estado

--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EstadoService } from '../../../services/estado.service';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-estado-form',
   standalone: true,
-  imports: [],
+  imports: [NgIf,ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './estado-form.component.html',
   styleUrl: './estado-form.component.css'
 })
 export class EstadoFormComponent {
-  exemploEstado: string = 'tocantins';
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+    private estadoService: EstadoService,
+    private router: Router ) {
+      this.formGroup = this.formBuilder.group({
+        nome:['', Validators.required],
+        sigla: ['', Validators.required]
+      })
+    }
+
+    OnSubmit(){
+      if(this.formGroup.valid){
+        const novoEstado = this.formGroup.value;
+        this.estadoService.salvar(novoEstado).subscribe({
+          next: (estadoCadastrado) => {
+            this.router.navigateByUrl('/estados');
+          },
+          error: (err) => {
+            console.log('Erro ao salvar' + JSON.stringify(err));
+          }
+        })
+      }
+    }
 }
--- src/app/services/estado.service.ts
+++ src/app/services/estado.service.ts
@@ -16,4 +16,8 @@ export class EstadoService {
     return this.httpClient.get<Estado[]>(this.baseUrl)
   }
 
+  salvar(estado: Estado): Observable<Estado>{
+    return this.httpClient.post<Estado>(this.baseUrl, estado);
+  }
+
 }
