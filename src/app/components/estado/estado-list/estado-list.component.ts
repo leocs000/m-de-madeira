120cce570edaafb5240cb202ff1fa1530976d472
leocs000 <leocsilva000@outlook.com>
2024-05-22 00:16:43 -0300

    adicionando o consumo de estados-list da API

--- src/app/components/estado/estado-list/estado-list.component.ts
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Estado } from '../../../models/estado.model';
+import { EstadoService } from '../../../services/estado.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-estado-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './estado-list.component.html',
   styleUrl: './estado-list.component.css'
 })
-export class EstadoListComponent {
+export class EstadoListComponent implements OnInit{
+  estados: Estado[] = [];
+
+  constructor(private estadoService: EstadoService) {}
+
+  ngOnInit(): void {
+    this.estadoService.getEstados().subscribe(data => {
+      this.estados = data;
+    });
+  }
 
 }
---
+++ src/app/services/estado.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Estado } from '../models/estado.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadoService {
+  private baseUrl = 'http://localhost:8080/estados'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEstados(): Observable<Estado[]> {
+    return this.httpClient.get<Estado[]>(this.baseUrl)
+  }
+
+}
