7d53575cf73d76c77133d60c27dc9723ca05a7df
LEONARDO CARVALHO SILVA <leonardo.carvalho@uniggelsementes.com.br>
2024-06-13 17:29:54 -0300

    alterando visual da listagem de produtos

--- src/app/components/madeira/madeira-list/madeira-list.component.ts
+++ src/app/components/madeira/madeira-list/madeira-list.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,11 +7,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { Madeira } from '../../../models/madeira.model';
 import { MadeiraService } from '../../../services/madeira.service';
+import { MatMenuModule } from '@angular/material/menu'; 
 
 @Component({
   selector: 'app-madeira-list',
   standalone: true,
-  imports: [NgFor, MatButtonModule, RouterModule, MatIconModule, MatTableModule, MatToolbarModule],
+  imports: [NgFor, MatButtonModule, RouterModule, MatIconModule, MatTableModule, MatToolbarModule, MatMenuModule, CommonModule],
   templateUrl: './madeira-list.component.html',
   styleUrl: './madeira-list.component.css'
 })
